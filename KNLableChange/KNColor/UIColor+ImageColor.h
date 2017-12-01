df008d7a149957565ccb9bda34a28f0877baa25c
Leessang <992275294@qq.com>
2017-12-01 11:01:40 +0800

    上传一个颜色分类
    这个分类的作用。用于获取图片的主色调然后返回颜色

---
+++ KNLableChange/KNColor/UIColor+ImageColor.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+ImageColor.h
+//  KNLableChange
+//
+//  Created by 刘凡 on 2017/12/1.
+//  Copyright © 2017年 KrystalName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (ImageColor)
+
++(UIColor*)getImageMainColor:(UIImage*)image;
+
+@end
